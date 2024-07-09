ee8cef790dab79b33d369fd717bc14c83290d476
rajacs50 <56812923+rajacs50@users.noreply.github.com>
2024-07-09 11:43:28 +0200

    Update next.config.js

--- site/next.config.js
+++ site/next.config.js
@@ -4,8 +4,8 @@ const nextConfig = {
   env: {
     name: 'Maltego Technologies',
     description: 'The official store for Kasm supported workspaces for Maltego Developers.',
-    icon: '/img/logo.svg',
-    listUrl: 'https://github.com/rajacs50/kasm-registry/',
+    icon: 'https://rajacs50.github.io/kasm-registry//1.0/icon.png',
+    listUrl: 'https://rajacs50.github.io/kasm-registry/',
     contactUrl: 'https://github.com/rajacs50/kasm-registry/issues',
   },
   reactStrictMode: true,
