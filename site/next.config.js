c7b22bd97272943e59bb30fe4d3915d88df6d7a6
rajacs50 <tedrajasummer-cs50@yahoo.com>
2024-07-16 11:35:33 +0200

    v1 Fixed issues: KASM Support rec

--- site/next.config.js
+++ site/next.config.js
@@ -4,7 +4,7 @@ const nextConfig = {
   env: {
     name: 'Maltego Technologies',
     description: 'The official store for Kasm supported workspaces for Maltego Developers.',
-    icon: 'https://rajacs50.github.io/kasm-registry//1.0/icon.png',
+    icon: 'https://www.maltego.com/img/Social%20Profile%20Pic@4x.png',
     listUrl: 'https://rajacs50.github.io/kasm-registry/',
     contactUrl: 'https://github.com/rajacs50/kasm-registry/issues',
   },
